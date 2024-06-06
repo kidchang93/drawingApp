a7af4a545a9ef76da89df9f16b3f1bb5db7d4f0d
kidchang93 <lck930721@gmail.com>
2024-06-06 16:36:02 +0900

    VO 설정

--- drawingApp_back/src/main/java/kr/co/drawing/drawingapp_back/vo/SocketVO.java
+++ drawingApp_back/src/main/java/kr/co/drawing/drawingapp_back/vo/SocketVO.java
@@ -1,4 +1,12 @@
 package kr.co.drawing.drawingapp_back.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SocketVO {
+    private String userName;
+    private String content;
+    private Object obj;
 }
